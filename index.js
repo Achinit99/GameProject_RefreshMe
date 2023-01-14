06d25c9b5013d77a0d2065c00a9ca82e368011d7
Achinit99 <achinit720@gmail.com>
2023-01-14 10:50:23 +0530

    If player 1 wins

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // 1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice-6.png
 
-var randomImageSource = "img/" + randomDiceImage;
+var randomImageSource = "img/" + randomDiceImage; // img/dice1.png - img/dice6.png
 
 var img1 = document.querySelectorAll("img")[0];
 
@@ -15,3 +15,12 @@ var randomImageSource2 = "img/dice" + randomNumber2 + ".png";
 var img1 = document
   .querySelectorAll("img")[1]
   .setAttribute("src", randomImageSource2);
+
+// If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Play 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
